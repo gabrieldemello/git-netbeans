33afb577c4973cf44b3e4bf0cc8482bbf71279fd
Gabriel de Mello e Silva Carneiro <35747250+gabrieldemello@users.noreply.github.com>
2020-12-01 03:00:06 -0300

    Workbench SQL
    Workbench SQL

---
+++ Lista01.sql
@@ -0,0 +1,23 @@
+SELECT * FROM lista.pessoa;
+
+use lista;
+select * from pessoa;
+select * from pessoafisica;
+select * from pessoajuridica;
+select * from lista.pessoafisica;
+select * from lista.pessoajuridica;
+
+-- select * from lista.pessoafisica;
+-- desc lista.pessoafisica;
+
+-- ALTER TABLE `lista`.`pessoa` 
+-- DROP COLUMN `Nome`,
+-- DROP PRIMARY KEY,
+-- ADD PRIMARY KEY (`idPessoa`);
+-- ;
+
+-- desc pessoajuridica;
+
+-- delete from pessoa;
+-- delete from pessoafisica;
+-- delete from pessoajuridica;
